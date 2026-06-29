d20bad83ce76c146ebcd70450c36cc51cd3faa72
tlacc <194394267+jeremydevv@users.noreply.github.com>
2026-06-28 23:55:15 -0400

    HTML Issue

--- src/app/legacy-html.tsx
+++
@@ -1,9 +0,0 @@
-import { legacyHtmlFor, type LegacyHtmlFile } from '../lib/legacy/html-source.ts';
-
-type LegacyHtmlProps = {
-  file: LegacyHtmlFile;
-};
-
-export default function LegacyHtml({ file }: LegacyHtmlProps) {
-  return <div dangerouslySetInnerHTML={{ __html: legacyHtmlFor(file) }} />;
-}
